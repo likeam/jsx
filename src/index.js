b267aa591f559ad5fe8bb414f8bbb7aa9f41fe63
AbdulRehman <likeam99@gmail.com>
2022-09-16 23:29:21 +0500

    Adding Css

--- src/index.js
+++ src/index.js
@@ -3,3 +3,18 @@ import React from "react";
 import ReactDOM from "react-dom"; 
 
 //Craate a react component
+const App = () => {
+    return (
+        <div>
+            <label class="label" for="name">
+                Enter name:
+            </label>
+            <input id="name" type="text" />
+            <button style= {{ backgroundColor: 'blue', color: 'white '}}>Submit</button>
+        </div>
+    );
+};
+
+//React Dom
+
+ReactDOM.render( <App />,document.querySelector('#root'));
