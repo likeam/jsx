0c645a5c0ea671a4cadd018ee693c398196b6adb
AbdulRehman <likeam99@gmail.com>
2022-09-22 20:38:19 +0500

    css setting

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import ReactDOM from "react-dom";
 //Craate a react component
 const App = () => {
     return (
-        <div style= {{ backgroundColor: 'blue', color: 'white '}}>
+        <div style= {{ backgroundColor: 'gray', color: 'black '}}>
             <label className="label" for="name">
                 Enter name:
             </label>
             <input id="name" type="text" />
-            <button style= {{ backgroundColor: 'red', color: 'white '}}>Submit</button>
+            <button style= {{ backgroundColor: 'gray', color: 'black '}}>Submit</button>
         </div>
     );
 };
